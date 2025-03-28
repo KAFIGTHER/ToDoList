69eb622022e9329dbf285dc34e2336f09344c249
kevinhanstein <kevin.hanstein@iamds.com>
2025-03-28 09:47:53 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import TodoList from "@/components/ToDoList.vue";
 </script>
 
